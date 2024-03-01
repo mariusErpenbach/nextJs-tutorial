7ed4f00431ae7a989ab83c428e961759ceaa4219
Hawty1 <marius_erpenbach@web.de>
2024-03-01 09:11:19 +0100

    optimizing fonts

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -4,9 +4,10 @@ import clsx from 'clsx';
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
-      className={clsx(
+      className={clsx( 
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
+          // use clsx to conditionally apply the classes, like this:
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
         },
